ac13465af8ea8ea14e0dbe324eddc508d10af028
Gabriel Sharpton <3bag@Tricias-MBP.lan1>
2019-08-12 16:21:03 -0500

    created game logic

---
+++ memorygame/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running.");
+var cardOne = "queen";
+	console.log("User flipped" + cardOne)
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+	console.log("User flipped" + cardFour)
