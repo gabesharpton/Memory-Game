b3f2003a2cf5b133fa668de46013195af1d5d314
Gabriel Sharpton <3bag@Tricias-MBP.lan1>
2019-08-13 00:57:38 -0500

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,7 +1,15 @@
-console.log("Up and running.");
-var cardOne = "queen";
-	console.log("User flipped" + cardOne)
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-	console.log("User flipped" + cardFour)
+var cards = ["queen", "king", "queen", "king"]
+
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("user flipped queen");
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("user flipped king");
+cardsInPlay.length;
+if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("you found a match")}
+	else{
+		alert("Try again")
+}
