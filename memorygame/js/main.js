9874d50bc8f5612335213e2b4e965c9ccf6830e9
Gabriel Sharpton <3bag@Tricias-MBP.lan1>
2019-08-13 01:51:11 -0500

    Objects

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,17 +1,40 @@
-var cards = ["queen", "king", "queen", "king"]
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardIamge: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+]
 var cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
 }
 function flipCard(cardId){
 
-cardsInPlay.push(cardId);
-console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cardId.rank);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].suit);
+console.log(cards[cardId].cardImage);
 checkForMatch();
 }
 flipCard(0);
